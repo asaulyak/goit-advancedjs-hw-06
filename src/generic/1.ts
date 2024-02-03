b15ccc126ba020c7ee54b8bc959834b86c691548
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-02-03 14:29:42 +0200

    complete task 2

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,17 +1,18 @@
 /*
-  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
+  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа.
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
+function getPromise(): Promise<Array<string | number>> {
+  return new Promise(resolve => {
     resolve(['Text', 50]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then(data => {
   console.log(data);
 });
 
-export {};
+export {
+  getPromise
+};
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -1,5 +1,5 @@
 /*
-  У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
+  У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType.
   Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
   Функція compare повинна повертати AllType.
 */
@@ -8,16 +8,21 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare(
+  top: Pick<AllType, 'name' | 'color'>,
+  bottom: Pick<AllType, 'position' | 'weight'>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
-export {};
+export {
+  compare
+};
